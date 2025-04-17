9cfbdb5c47534cb4720544ed3bc1629242509cd6
voidedstarlight <haii@voided.systems>
2025-04-16 20:46:28 -0700

    feat(web): page title and fix spacing

--- apps/fed-web/src/views/node/visualizers/hostname.ts
+++ apps/fed-web/src/views/node/visualizers/hostname.ts
@@ -2,8 +2,11 @@ import getHash from "../../../util/hash";
 
 function hostname(data: any, container: HTMLElement) {
 	const subtitle = document.getElementById("subtitle-host");
-	const text = `${data.hostname} * ${getHash()}`;
+	const text = `${data.hostname} - ${getHash()}`;
 	subtitle.innerText = text;
+
+	const title = document.getElementsByTagName("title")[0];
+	if (title) title.innerText = text;
 }
 
 export default hostname;
